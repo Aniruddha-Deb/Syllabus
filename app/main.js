2fe4f483a47e069280a3a3004fe8f58a9d4d5ef2
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2017-10-12 16:24:41 +0530

    Experimented a little with databases

--- app/main.js
+++ app/main.js
@@ -1,8 +1,23 @@
 var express = require( "express" );
+var mongoose = require( "mongoose" );
 var app = express();
 
+mongoose.connect( "mongodb://localhost/Syllabus" );
+var db = mongoose.connection;
+
+var Document = require( "./Document" );
+
+var doc = new Document();
+var item1 = new Document.Item( "Apple" );
+var item2 = new Document.Item( "Banana" );
+doc.addItem( item1 );
+doc.addItem( item2 );
+
+doc.removeItem( item1 );
+doc.save();
+
 app.get( "/", ( req, res ) => {
-    res.sendFile( __dirname + "/client/homePage.html" );
+    res.send( "<html><body>" + Document.find( {name: } ) + "</body></html>" );
 } );
 
 app.listen( 8080 );
